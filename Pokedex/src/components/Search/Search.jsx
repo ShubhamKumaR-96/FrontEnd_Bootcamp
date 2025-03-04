665c1d4f8eb59c56c04cb1a848c077d7cd761486
ShubhamKumaR-96 <shubhamkas245@gmail.com>
2025-03-04 17:28:29 +0530

    Pokedex UI completed

---
+++ Pokedex/src/components/Search/Search.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Search.css'
+
+const Search = () => {
+  return (
+    <div className='Search-wrapper'>
+      <input type='text' placeholder='pokemon name ...' id='pokedex-name-search' />
+    </div>
+  )
+}
+
+export default Search
